4c9dd77a7923f29ee710f1d213f3382dd3590f70
Muhammet Anıl Ceylan <muhammetanilceylann@gmail.com>
2026-04-01 00:13:06 +0300

    force rebuild cache

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+      }
+    }
   },
   server: {
     historyApiFallback: true
